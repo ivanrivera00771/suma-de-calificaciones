e1307e3b202a5a4fe39ddc76c2e5e99d080be843
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2020-12-16 18:32:39 -0600

    suma de calificaciones sin salto de pagina

---
+++ src/introducircalificaciones/IntroducirCalificaciones.java
@@ -0,0 +1,72 @@
+
+package introducircalificaciones;
+
+import java.awt.BorderLayout;
+import java.util.Scanner;
+
+
+public class IntroducirCalificaciones {
+
+    
+    public static void main(String[] args) {
+        
+      
+                
+        
+        
+        System.out.println("Introduce el primer numero");
+        Scanner one = new Scanner(System.in);
+        double uno = one.nextDouble();
+        
+        System.out.println("Introduce el segundo numero");
+        Scanner two = new Scanner(System.in);
+        double dos = two.nextDouble();
+        
+        System.out.println("Introduce el tercer numero");
+        Scanner three = new Scanner(System.in);
+        double tres = three.nextDouble();
+        
+        double suma = uno+dos+tres;
+        
+        System.out.println("la suma de las calificaciones es " + suma);
+        
+        ///// otra forma
+         
+        double nota1, nota2, nota3, sumas;
+        System.out.println("INTRODUCIR NOTAS");        
+        Scanner notas = new Scanner(System.in);
+        
+        nota1= notas.nextDouble();
+        nota2= notas.nextDouble();
+        nota3= notas.nextDouble();
+        
+        sumas = nota1+nota2+nota3;
+        
+        System.out.println("Las calificaciones son " +suma);
+        
+       
+                
+                
+        
+        
+        
+        
+       
+        
+        
+        
+        
+       
+     
+       
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
